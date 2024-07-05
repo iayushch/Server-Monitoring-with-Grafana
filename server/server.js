00725b6fb91cc8eae2adc3fbd5e393d1568a571b
iayushch <ayush345kr@gmail.com>
2024-07-05 13:21:23 +0530

    chore: Add initial server setup

---
+++ server/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { json } from 'stream/consumers';
+
+const app = express();
+app.use(express.json());
+dotenv.config();
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Server is running' });
+});
+
+app.get('/slow',async (req, res) => {
+    try{
+        const timeTaken = await doSomeHeavyTask();
+        return res.json({
+            status: 'success',
+            message: `Task completed in ${timeTaken} ms`
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: 'error',
+            message: error.message
+        });
+    }
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
